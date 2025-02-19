15b65d29022f9502fdf8defba5275ccb0710921a
sbakter <sbakter@gmail.com>
2025-02-20 00:54:48 +0500

    8.2 work

--- src/routes/SinglePage.jsx
+++ src/routes/SinglePage.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { Box, Typography, CircularProgress, Chip, Rating } from '@mui/material';
+import { useEffect } from 'react';
+import { Box, Typography, CircularProgress, Chip, Rating, Card, CardMedia, CardContent } from '@mui/material';
 import useAxios from '../services/useAxios';
 
 /**
@@ -18,32 +18,37 @@ function SinglePage() {
   }, [id]);
 
   return (
-    <Box sx={{ mx: 'auto', p: 2 }}>
+    <Box sx={{ mx: 'auto', p: 2, maxWidth: 800 }}>
       {loading && <CircularProgress />}
       {!loading && book && (
-        <Box>
-          <Typography variant="h4" component="h1">
-            {book.name}
-          </Typography>
-          <Typography variant="h6" component="h2">
-            {book.author}
-          </Typography>
-          <Box sx={{ display: 'flex', alignItems: 'center', mt: 2 }}>
-            <Rating name="read-only" value={book.stars} readOnly size="large" />
-            <Box sx={{ ml: 2 }}>{book.stars}</Box>
-          </Box>
-          <Box sx={{ mt: 2 }}>
-            {book.genres.map((genre, i) => (
-              <Chip key={i} label={genre} variant="outlined" size="small" />
-            ))}
-          </Box>
-          <Box sx={{ mt: 2 }}>
-            <img src={book.img} alt={book.name} style={{ maxWidth: '100%' }} />
-          </Box>
-          <Typography variant="body1" sx={{ mt: 2 }}>
-            {book.description}
-          </Typography>
-        </Box>
+        <Card>
+          <CardMedia
+            component="img"
+            height="400"
+            image={book.img}
+            alt={book.name}
+          />
+          <CardContent>
+            <Typography variant="h4" component="h1" gutterBottom>
+              {book.name}
+            </Typography>
+            <Typography variant="h6" component="h2" gutterBottom>
+              {book.author}
+            </Typography>
+            <Box sx={{ display: 'flex', alignItems: 'center', mt: 2 }}>
+              <Rating name="read-only" value={book.stars} readOnly size="large" />
+              <Box sx={{ ml: 2 }}>{book.stars}</Box>
+            </Box>
+            <Box sx={{ mt: 2 }}>
+              {book.genres.map((genre, i) => (
+                <Chip key={i} label={genre} variant="outlined" size="small" sx={{ mr: 1, mb: 1 }} />
+              ))}
+            </Box>
+            <Typography variant="body1" sx={{ mt: 2 }}>
+              {book.description}
+            </Typography>
+          </CardContent>
+        </Card>
       )}
     </Box>
   );
